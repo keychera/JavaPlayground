8e473d47b86e4ce734f536da457581d58bb62a3f
keychera <kyogatama@gmail.com>
2021-07-05 10:20:05 +0700

    add maven plugins and use the Builder

--- test/src/main/java/App.java
+++ test/src/main/java/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        var person = new AnnotationUser();
+        var personBuilder = new AnnotationUserBuilder();
+        var person = personBuilder.setData1(1).setData2(2).build();
+        System.out.println(person.data1);
     }
 }
